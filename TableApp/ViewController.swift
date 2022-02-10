8f1a54a387bc5446b2c8519b0231d70655fa06fa
Raman Novakhatski <romabelarus@mail.ru>
2022-02-10 13:56:23 +0300

    Initial Commit

---
+++ TableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableApp
+//
+//  Created by Roma on 10.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
